b0053dccf1bd153f26ff572e60e175ba14ec3417
siddhesh jadhav <siddheshvjadhav20@gmail.com>
2024-10-08 21:49:21 +0530

    Auth routes

--- middleware/protectRoute.ts
+++ middleware/protectRoute.ts
@@ -6,6 +6,9 @@ interface DecodedToken extends JwtPayload {
 	userId: string;
 }
 
+ git config --global user.email "you@example.com"
+  git config --global user.name "Your Name"
+
 declare global {
 	namespace Express {
 		export interface Request {
